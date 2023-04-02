9ea450e01018b1f3a15aa45803184c7c6f421862
przemekkapica <przemekapica@gmail.com>
2023-04-02 13:16:41 +0200

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Przemysław Kapica on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
